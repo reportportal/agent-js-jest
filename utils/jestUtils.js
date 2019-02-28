38ec030f9ef4478cb80114e11f7792824855e532
vlad <fizik200903@gmail.com>
2019-02-28 15:43:48 +0300

    Fixed file dependencies

--- utils/jestUtils.js
+++ utils/jestUtils.js
@@ -1,11 +1,11 @@
 function _interopRequireDefault(obj) { 
-    return obj && obj.__esModule ? obj : { default: obj }; 
+  return obj && obj.__esModule ? obj : { default: obj }; 
 }
 
 function load_base_reporter() {
-  return _base_reporter = _interopRequireDefault(require('../../jest/node_modules/jest-cli/build/reporters/base_reporter'));
+  return _base_reporter = _interopRequireDefault(require('../../../jest/node_modules/jest-cli/build/reporters/base_reporter'));
 }
 
 module.exports = {
-    load_base_reporter
+  load_base_reporter
 }
