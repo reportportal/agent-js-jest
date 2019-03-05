7a18440fc9a7ea8ca15fbef21966881fd8312550
vlad <fizik200903@gmail.com>
2019-03-05 13:43:17 +0300

    Updated package version to 1.0.5. Fixed reading of  env variables

--- utils/objectUtils.js
+++ utils/objectUtils.js
@@ -1,14 +1,6 @@
 /* eslint-disable no-process-env */
 const entityType = { SUITE: 'SUITE', TEST: 'STEP' },
 
-    getClientInitObject = (options = {}) => ({
-        token: process.env.RP_TOKEN,
-        endpoint: options.endpoint,
-        launch: process.env.RP_LAUNCH_NAME || options.launchname || 'Unit Tests',
-        project: process.env.RP_PROJECT_NAME || options.project,
-        tags: process.env.RP_TAGS.split(',') || options.tags || 'tag1, tag2'
-    }),
-
     getStartLaunchObject = (options = {}) => ({
         launch: process.env.RP_LAUNCH_NAME || options.launchname || 'Unit Tests',
         tags: options.tags,
@@ -24,7 +16,19 @@ const entityType = { SUITE: 'SUITE', TEST: 'STEP' },
         type: entityType.SUITE,
         name: suiteName || 'Title Custom',
         start_time: new Date().valueOf()
-    });
+    }),
+
+    getClientInitObject = (options = {}) => {
+        let env_tags = process.env.RP_TAGS === undefined ? undefined : process.env.RP_TAGS.split(',');
+
+        return {
+            token: process.env.RP_TOKEN,
+            endpoint: options.endpoint,
+            launch: process.env.RP_LAUNCH_NAME || options.launchname || 'Unit Tests',
+            project: process.env.RP_PROJECT_NAME || options.project,
+            tags: env_tags || options.tags || 'tag1, tag2'
+        };
+    };
 
 module.exports = {
     getClientInitObject,
