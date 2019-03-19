0db5b15d0a7a3920c2acb41285f9820a54efc11b
Travis Mortimer <tmortimer@melaleuca.com>
2019-03-18 18:01:50 -0600

    added additional possible path to base_reporter in jestUtils with backwards compatibility

--- utils/jestUtils.js
+++ utils/jestUtils.js
@@ -1,8 +1,26 @@
+const possibleBaseReporterPaths = [
+    '../../../@jest/reporters/build/base_reporter', // >=v24.0.0 of jest (verified through 24.5.0)
+    '../../../jest/node_modules/jest-cli/build/reporters/base_reporter' // <=v23.6.0 of jest
+];
 // eslint-disable-next-line no-confusing-arrow
 const _interopRequireDefault = obj => obj && obj.__esModule ? obj : { default: obj },
 
     load_base_reporter = () => {
-        const base_reporter = require('../../../jest/node_modules/jest-cli/build/reporters/base_reporter');
+        const fs = require('fs');
+        const path = require('path');
+        var requirePath;
+        for (let filePath of possibleBaseReporterPaths) {
+            filePath = path.join(__dirname, filePath);
+            if (fs.existsSync(`${filePath}.js`)) {
+                requirePath = filePath;
+                break;
+            }
+        }
+        if (requirePath === undefined) {
+            throw new Error(
+                `base_reporter.js not found at any of the possible paths:\n${possibleBaseReporterPaths.join('\n')}`);
+        }
+        const base_reporter = require(requirePath);
         let result = _interopRequireDefault(base_reporter);
 
         return result;
