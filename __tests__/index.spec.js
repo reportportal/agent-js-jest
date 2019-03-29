770731f5d267ea136ee78719b6cceb0c2b826410
vlad <fizik200903@gmail.com>
2019-03-29 14:35:31 +0300

    Added test for check required rp client package

---
+++ __tests__/index.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const clientRPRestler = require('reportportal-client-restler');
+
+
+describe('index script', () => {
+    test('require report-portal-restler package', () => {
+        const result = new clientRPRestler({
+            debug: false,
+            token: "00000000-0000-0000-0000-000000000000",
+            endpoint: "http://your-instance.com:8080/api/v1",
+            launch: "LAUNCH_NAME",
+            project: "PROJECT_NAME"
+        });
+
+        expect(result).toBeDefined();
+        expect(result).not.toBe(null);
+    });
+});
