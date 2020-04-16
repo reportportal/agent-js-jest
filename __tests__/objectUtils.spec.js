1a7810f299aeedc2803d55955806421530652323
Katsiaryna Tatarynovich <Katsiaryna_Tatarynovich@epam.com>
2020-04-16 13:58:46 +0300

    EPMRPP-49769: [AGENT] Jest. Implement a collecting of Google analytics event: Name and version of Agent

--- __tests__/objectUtils.spec.js
+++ __tests__/objectUtils.spec.js
@@ -2,7 +2,8 @@
 const { getClientInitObject,
     getStartLaunchObject,
     getSuiteStartObject,
-    getTestStartObject } = require('./../utils/objectUtils');
+    getTestStartObject,
+    getAgentInfo } = require('./../utils/objectUtils');
 const pjson = require('./../package.json');
 const defaultOptions = {
     launch: 'launchName',
@@ -44,117 +45,134 @@ describe('Object Utils script', () => {
         process.env = OLD_ENV;
     });
 
-    test('should return start launch object with correct values', () => {
-        const expectedStartLaunchObject = {
-            launch: 'launchName',
-            description: 'description',
-            attributes: [
-                {
-                    'key': 'YourKey',
-                    'value': 'YourValue'
-                }, {
-                    'value': 'YourValue'
-                }, {
-                    key: 'agent',
-                    value: `${pjson.name}|${pjson.version}`,
-                    system: true,
-                },
-            ],
-            rerun: true,
-            rerunOf: '00000000-0000-0000-0000-000000000000',
-            startTime: new Date().valueOf(),
-        };
+    describe('getStartLaunchObject', function () {
+        test('should return start launch object with correct values', () => {
+            const expectedStartLaunchObject = {
+                launch: 'launchName',
+                description: 'description',
+                attributes: [
+                    {
+                        'key': 'YourKey',
+                        'value': 'YourValue'
+                    }, {
+                        'value': 'YourValue'
+                    }, {
+                        key: 'agent',
+                        value: `${pjson.name}|${pjson.version}`,
+                        system: true,
+                    },
+                ],
+                rerun: true,
+                rerunOf: '00000000-0000-0000-0000-000000000000',
+                startTime: new Date().valueOf(),
+            };
 
-        const startLaunchObject = getStartLaunchObject(defaultOptions);
+            const startLaunchObject = getStartLaunchObject(defaultOptions);
 
-        expect(startLaunchObject).toBeDefined();
-        expect(startLaunchObject).toEqual(expectedStartLaunchObject);
+            expect(startLaunchObject).toBeDefined();
+            expect(startLaunchObject).toEqual(expectedStartLaunchObject);
+        });
+
+        test('should return start launch object with default values if options don\'t set', () => {
+            const expectedStartLaunchObject = {
+                launch: 'Unit Tests',
+                attributes: [
+                    {
+                        key: 'agent',
+                        value: `${pjson.name}|${pjson.version}`,
+                        system: true,
+                    }
+                ],
+                startTime: new Date().valueOf(),
+            };
+
+            const startLaunchObject = getStartLaunchObject();
+
+            expect(startLaunchObject).toBeDefined();
+            expect(startLaunchObject).toEqual(expectedStartLaunchObject);
+        });
     });
 
-    test('should return start launch object with default values if options don\'t set', () => {
-        const expectedStartLaunchObject = {
-            launch: 'Unit Tests',
-            attributes: [
-                {
-                    key: 'agent',
-                    value: `${pjson.name}|${pjson.version}`,
-                    system: true,
-                }
-            ],
-            startTime: new Date().valueOf(),
-        };
+    describe('getTestStartObject', function () {
+        test('should return test start object with correct values', () => {
+            const expectedTestStartObject = {
+                type: 'STEP',
+                name: 'test title',
+                retry: true
+            };
 
-        const startLaunchObject = getStartLaunchObject();
+            const testStartObject = getTestStartObject('test title', true);
 
-        expect(startLaunchObject).toBeDefined();
-        expect(startLaunchObject).toEqual(expectedStartLaunchObject);
+            expect(testStartObject).toBeDefined();
+            expect(testStartObject).toEqual(expectedTestStartObject);
+        });
     });
 
-    test('should return test start object with correct values', () => {
-        const expectedTestStartObject = {
-            type: 'STEP',
-            name: 'test title',
-            retry: true
-        };
+    describe('getSuiteStartObject', function () {
+        test('should return suite start object with correct values', () => {
+            const expectedSuiteStartObject = {
+                type: 'SUITE',
+                name: 'suite name',
+                startTime: new Date().valueOf(),
+            };
 
-        const testStartObject = getTestStartObject('test title', true);
+            const suiteStartObject = getSuiteStartObject('suite name');
 
-        expect(testStartObject).toBeDefined();
-        expect(testStartObject).toEqual(expectedTestStartObject);
+            expect(suiteStartObject).toBeDefined();
+            expect(suiteStartObject).toEqual(expectedSuiteStartObject);
+        });
     });
 
-    test('should return suite start object with correct values', () => {
-        const expectedSuiteStartObject = {
-            type: 'SUITE',
-            name: 'suite name',
-            startTime: new Date().valueOf(),
-        };
+    describe('getClientInitObject', function () {
+        test('should return client init object with correct values, some parameters taken from environment variables', () => {
+            process.env = {
+                RP_TOKEN: '00000000-0000-0000-0000-000000000000',
+                RP_LAUNCH: 'launch name',
+                RP_PROJECT_NAME: 'project name',
+                RP_ATTRIBUTES: 'attributesOne,attributesTwoKey:attributesTwoValue',
 
-        const suiteStartObject = getSuiteStartObject('suite name');
+            };
+            const expectedClientInitObject = {
+                token: '00000000-0000-0000-0000-000000000000',
+                endpoint: 'endpoint',
+                launch: 'launch name',
+                project: 'project name',
+                rerun: true,
+                rerunOf: '00000000-0000-0000-0000-000000000000',
+                description: 'description',
+                attributes: [{ key: null, value: 'attributesOne' }, { key: 'attributesTwoKey', value: 'attributesTwoValue' }]
+            };
+            const options = {
+                endpoint: 'endpoint',
+                rerun: true,
+                rerunOf: '00000000-0000-0000-0000-000000000000',
+                description: 'description'
+            };
 
-        expect(suiteStartObject).toBeDefined();
-        expect(suiteStartObject).toEqual(expectedSuiteStartObject);
+            const clientInitObject = getClientInitObject(options);
+
+            expect(clientInitObject).toBeDefined();
+            expect(clientInitObject).toEqual(expectedClientInitObject);
+        });
+
+        test('should return client init object with default values if options don\'t set', () => {
+            const expectedClientInitObject = {
+                launch: 'Unit Tests',
+            };
+
+            const clientInitObject = getClientInitObject();
+
+            expect(clientInitObject).toBeDefined();
+            expect(clientInitObject).toEqual(expectedClientInitObject);
+        });
     });
 
-    test('should return client init object with correct values, some parameters taken from environment variables', () => {
-        process.env = {
-            RP_TOKEN: '00000000-0000-0000-0000-000000000000',
-            RP_LAUNCH: 'launch name',
-            RP_PROJECT_NAME: 'project name',
-            RP_ATTRIBUTES: 'attributesOne,attributesTwoKey:attributesTwoValue',
+    describe('getAgentInfo', function() {
+        test('should return the name and version of application from package.json file', function () {
+            const agentInfo = getAgentInfo();
 
-        };
-        const expectedClientInitObject = {
-            token: '00000000-0000-0000-0000-000000000000',
-            endpoint: 'endpoint',
-            launch: 'launch name',
-            project: 'project name',
-            rerun: true,
-            rerunOf: '00000000-0000-0000-0000-000000000000',
-            description: 'description',
-            attributes: [{ key: null, value: 'attributesOne' }, { key: 'attributesTwoKey', value: 'attributesTwoValue' }]
-        };
-        const options = {
-            endpoint: 'endpoint',
-            rerun: true,
-            rerunOf: '00000000-0000-0000-0000-000000000000',
-            description: 'description'
-        };
-
-        const clientInitObject = getClientInitObject(options);
-
-        expect(clientInitObject).toBeDefined();
-        expect(clientInitObject).toEqual(expectedClientInitObject);
-    });
-
-    test('should return client init object with default values if options don\'t set', () => {
-        const expectedClientInitObject = {
-            launch: 'Unit Tests',
-        };
-
-        const clientInitObject = getClientInitObject();
-
-        expect(clientInitObject).toBeDefined();
-        expect(clientInitObject).toEqual(expectedClientInitObject);
+            expect(agentInfo.name).toBe(pjson.name);
+            expect(agentInfo.version).toBe(pjson.version);
+        });
     });
 });
